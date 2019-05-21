c4deebba5936ae3ac6c9def9b696dbc4c6fccdac
Jaz Lalli <jazlalli@hotmail.com>
2019-05-21 13:20:17 +0100

    Chpt 3.1 - Variables and Mutability

---
+++ variables/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("the value of x is {}", x);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("the number of spaces is {}", spaces);
+}
