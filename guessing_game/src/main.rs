24b3ffc69aafd89603946a8ead389ee23e73e975
Jaz Lalli <jazlalli@hotmail.com>
2019-05-21 13:25:18 +0100

    Chpt 2 - Guessing Game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,33 @@
+extern crate rand;
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Time to guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+      let mut guess = String::new();
+
+      println!("Input your guess?");
+
+      io::stdin().read_line(&mut guess)
+        .expect("Failed to read your input");
+
+      let guess: u32 = match guess.trim().parse() {
+          Ok(num) => num,
+          Err(_) => continue,
+      };
+
+      match guess.cmp(&secret_number) {
+          Ordering::Less => println!("Too small!"),
+          Ordering::Greater => println!("Too big!"),
+          Ordering::Equal => {
+            println!("You win!");
+            break;
+          }
+      };
+    }
+}
