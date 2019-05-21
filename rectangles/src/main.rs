fc29558dbfd8eee1e25ea073bea87f96f600a1c5
Jaz Lalli <jazlalli@hotmail.com>
2019-05-21 21:49:31 +0100

    Chpt 5.3 - Structs and Methods

---
+++ rectangles/src/main.rs
@@ -0,0 +1,32 @@
+// use Debug trait
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    // methods, get immutable reference to the instance
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!(
+      "Rects: \n{:#?} \n{:#?} \n{:#?}",
+      rect1, rect2, rect3
+    );
+
+    println!("The area of rect1 is {} square pixels.", rect1.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
